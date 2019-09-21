389488b7c043bddd6e1d3abbad5af2019a9b728a
ellnw <el_2542@hotmail.com>
2019-09-21 04:15:33 -0700

    card-game

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import WordCard from './WordCard';
+//import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <WordCard value="THANAWUT"/>    
+      </div>
+    );
+  }
+}
+
+export default App;
